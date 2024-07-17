0525cf7a72fecb72677439d45e6df9c949684c7a
Sean Howe <showe1364@gmail.com>
2024-07-16 20:25:52 -0400

    tempting to restart

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { sveltePreprocess } from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: sveltePreprocess(),
 
 	kit: {
 		adapter: adapter({
